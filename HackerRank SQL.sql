85906e0955ff24d9da435ae85658116f9653153d
Martin Yeghiazaryan <martineghiazaryan@gmail.com>
2023-06-21 15:23:42 +0200

    HackerRank added

--- HackerRank SQL.sql
+++ HackerRank SQL.sql
@@ -8,5 +8,23 @@
 -- ORDER BY LENGTH(CITY) DESC, CITY ASC
 -- LIMIT 1;
 
+-- CITY names starting with vowels 'a','e','i','o','u'
 
+-- SELECT DISTINCT CITY
+-- FROM STATION
+-- WHERE LEFT(CITY,1) IN ('a','e','i','o','u');
+
+-- SELECT DISTINCT CITY
+-- FROM STATION
+-- WHERE LEFT(CITY,1) IN ('a','e','i','o','u') AND RIGHT(CITY,1) IN ('a','e','i','o','u') ;
+
+
+-- SELECT name
+-- FROM Employee
+-- WHERE salary > 2000 AND months < 10
+-- ORDER BY employee_id ASC; 
+
+
+-- Students who score 75, sort them by last 3 caracters in name and secondary sort them by ascending id
+-- select name from students where marks > 75 order by right(name,3),id asc;
 
